20cfa5dc187e691fc7f4184a8f7fca0e4b540834
Tamas Piski <tpiski@gmail.com>
2019-10-30 12:26:49 +0100

    advanced done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Vehicle = /** @class */ (function () {
     function Vehicle(description, vehicleType, engineType, odometer, seats, fuelType, year) {
         this.description = description;
@@ -11,7 +24,39 @@ var Vehicle = /** @class */ (function () {
     Vehicle.prototype.greet = function () {
         return "Hello, " + this.description;
     };
+    Vehicle.prototype.basePrice = function () {
+        return (this.seats * 1000) / (this.odometer / 100) - (2019 - this.year);
+    };
     return Vehicle;
 }());
+var Motor = /** @class */ (function (_super) {
+    __extends(Motor, _super);
+    function Motor(description, vehicleType, engineType, odometer, seats, fuelType, year, hasTouringBox) {
+        var _this = _super.call(this, description, vehicleType, engineType, odometer, seats, fuelType, year) || this;
+        _this.hasTouringBox = hasTouringBox;
+        return _this;
+    }
+    Motor.prototype.retailPrice = function () {
+        var basePrice = _super.prototype.basePrice.call(this);
+        if (this.hasTouringBox) {
+            return basePrice + 100;
+        }
+        else {
+            return basePrice - 50;
+        }
+    };
+    return Motor;
+}(Vehicle));
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(description, vehicleType, engineType, odometer, seats, fuelType, year, axleNumber) {
+        var _this = _super.call(this, description, vehicleType, engineType, odometer, seats, fuelType, year) || this;
+        _this.axleNumber = axleNumber;
+        return _this;
+    }
+    return Truck;
+}(Vehicle));
 var Ferrari = new Vehicle("Cool", "Car", "IC", 300, 2, "Petrol", 2010);
-console.log(Ferrari.greet());
+var Suzuki = new Motor("cooler", "motor", "IC", 100, 1, "petrol", 2012, false);
+var Man = new Truck("cooler", "truck", "IC", 100, 1, "diesel", 2012, 6);
+console.log(Suzuki.retailPrice());
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,11 +1,11 @@
 class Vehicle {
-    description;
-    vehicleType;
-    engineType;
-    odometer;
-    seats;
-    fuelType;
-    year;
+    description :string;
+    vehicleType : string;
+    engineType : string;
+    odometer : number;
+    seats : number;
+    fuelType : string;
+    year : number;
 
     constructor(description: string, vehicleType: string, engineType : string, odometer: number, seats: number, fuelType: string, year: number) {
         this.description = description;
@@ -20,8 +20,47 @@ class Vehicle {
     greet() {
         return "Hello, " + this.description;
     }
+
+    basePrice() {
+        return (this.seats*1000)/(this.odometer/100)-(2019-this.year)
+
+    }
+}
+
+class Motor extends Vehicle {
+    hasTouringBox : boolean;
+
+    constructor(description: string, vehicleType: string, engineType : string, odometer: number, seats: number, fuelType: string, year: number, hasTouringBox: boolean) {
+
+    super(description,vehicleType, engineType, odometer, seats, fuelType, year)
+    this.hasTouringBox = hasTouringBox;
+    }
+    
+    retailPrice() {
+        let basePrice : number = super.basePrice()
+        if (this.hasTouringBox) {
+            return basePrice + 100;
+        } 
+        else {
+            return basePrice-50;
+        }
+    }
+    
+}
+
+class Truck extends Vehicle {
+    axleNumber : number;
+
+    constructor(description: string, vehicleType: string, engineType : string, odometer: number, seats: number, fuelType: string, year: number, axleNumber: number) {
+
+    super(description,vehicleType, engineType, odometer, seats, fuelType, year)
+    this.axleNumber = axleNumber;
+    }
 }
 
 let Ferrari = new Vehicle("Cool", "Car", "IC", 300, 2, "Petrol", 2010);
+let Suzuki = new Motor("cooler", "motor", "IC", 100, 1, "petrol", 2012, false);
+let Man = new Truck("cooler", "truck", "IC", 100, 1, "diesel", 2012, 6);
 
-console.log(Ferrari.greet())
+
+console.log(Suzuki.retailPrice())
